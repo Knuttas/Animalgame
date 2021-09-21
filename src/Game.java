d870491b7d657f3ad06c8490e3cc36a36aec1d82
Oskar Knutsson <oskar.knutsson@gmail.com>
2021-09-21 13:34:03 +0200

    Menu updated

--- src/Game.java
+++ src/Game.java
@@ -28,35 +28,48 @@ public class Game {
             for (int x = 0; x < players.length; x++) {
                 System.out.println(players[x].getPlayerName() + "'s turn.");
                 System.out.println("Please choose one of the following: ");
-                System.out.println("1. Buy animal");
-                System.out.println("2. Buy food");
-                System.out.println("3. Feed animals");
-                System.out.println("4. Breed animals");
-                System.out.println("5. Sell animals");
+                System.out.println("1. Buy animal  2. Buy food  3. Feed animals  4. Breed animals  5. Sell animals");
                 int choice = myScanner.nextInt();
                 if (choice == 1){
                     System.out.println("What animal do you want to buy?");
-                    System.out.println("1. Cow");
-                    System.out.println("2. Dog");
-                    System.out.println("3. Horse");
-                    System.out.println("4. Lizard");
-                    System.out.println("5. Sheep");
+                    System.out.println("1. Cow  2. Dog  3. Horse  4. Lizard  5. Sheep");
                     int animalChoice = myScanner.nextInt();
+                    int amountOfAnimal;
                     if (animalChoice == 1){
                         System.out.println("How many cows do you want to buy?");
+                        amountOfAnimal = myScanner.nextInt();
                     } else if (animalChoice == 2) {
                         System.out.println("How many dogs do you want to buy?");
+                        amountOfAnimal = myScanner.nextInt();
                     } else if (animalChoice == 3) {
                         System.out.println("How many horses do you want to buy?");
+                        amountOfAnimal = myScanner.nextInt();
                     } else if (animalChoice == 4) {
                         System.out.println("How many Lizards do you want to buy?");
+                        amountOfAnimal = myScanner.nextInt();
                     } else if (animalChoice == 5) {
-                        System.out.println("How many sheeps do you want to buy?");
+                        System.out.println("How many sheep do you want to buy?");
+                        amountOfAnimal = myScanner.nextInt();
                     } else {
                         System.out.println("Invalid choice");
                     }
                 } else if (choice == 2) {
-                    System.out.println("you bought food");
+                    System.out.println("What food do you want to buy?");
+                    System.out.println("1. Hay  2. Meat  3. Salad");
+                    int foodChoice = myScanner.nextInt();
+                    int amountOfFood;
+                    if (foodChoice == 1) {
+                        System.out.println("How many kg of hay do you want to buy?");
+                        amountOfFood = myScanner.nextInt();
+                    } else if (foodChoice == 2) {
+                        System.out.println("How many kg of meat do you want to buy?");
+                        amountOfFood = myScanner.nextInt();
+                    } else if (foodChoice == 3) {
+                        System.out.println("How many kg of salad do you want to buy?");
+                        amountOfFood = myScanner.nextInt();
+                    } else {
+                        System.out.println("Invalid choice");
+                    }
                 } else if (choice == 3) {
                     System.out.println("you fed an animal");
                 } else if (choice == 4) {
