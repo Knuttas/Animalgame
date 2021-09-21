22894fd329be57f82341e06fcad9569bb62e55f0
Oskar Knutsson <oskar.knutsson@gmail.com>
2021-09-21 13:10:54 +0200

    Improved menu

--- src/Game.java
+++ src/Game.java
@@ -35,7 +35,26 @@ public class Game {
                 System.out.println("5. Sell animals");
                 int choice = myScanner.nextInt();
                 if (choice == 1){
-                    System.out.println("you bought an animal");
+                    System.out.println("What animal do you want to buy?");
+                    System.out.println("1. Cow");
+                    System.out.println("2. Dog");
+                    System.out.println("3. Horse");
+                    System.out.println("4. Lizard");
+                    System.out.println("5. Sheep");
+                    int animalChoice = myScanner.nextInt();
+                    if (animalChoice == 1){
+                        System.out.println("How many cows do you want to buy?");
+                    } else if (animalChoice == 2) {
+                        System.out.println("How many dogs do you want to buy?");
+                    } else if (animalChoice == 3) {
+                        System.out.println("How many horses do you want to buy?");
+                    } else if (animalChoice == 4) {
+                        System.out.println("How many Lizards do you want to buy?");
+                    } else if (animalChoice == 5) {
+                        System.out.println("How many sheeps do you want to buy?");
+                    } else {
+                        System.out.println("Invalid choice");
+                    }
                 } else if (choice == 2) {
                     System.out.println("you bought food");
                 } else if (choice == 3) {
